b1c242a051ccc703379136f9d52f82f189605ac4
Fabian Emmel <emmelnem@gmail.com>
2020-12-02 14:51:39 +0100

    Day 1+2

--- AdventOfCode/Day_01.cs
+++ AdventOfCode/Day_01.cs
@@ -1,19 +1,55 @@
 ﻿using AoCHelper;
 using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode
 {
     public class Day_01 : BaseDay
     {
-        private readonly string _input;
+        private readonly List<int> _input;
+        private const int target = 2020;
 
         public Day_01()
         {
-            _input = File.ReadAllText(InputFilePath);
+            _input = File.ReadAllLines(InputFilePath).Select(int.Parse).ToList();
+        }
+        public override string Solve_1()
+        {
+            var data = _input;
+            data.Sort();
+
+            var lower = 0;
+            var upper = data.Count - 1;
+
+            while (lower < upper)
+            {
+                if (data.ElementAt(lower) + data.ElementAt(upper) == target)
+                    return (data.ElementAt(lower) * data.ElementAt(upper)).ToString();
+                if (data.ElementAt(lower) + data.ElementAt(upper) < target) lower++; else upper--;
+            }
+            return "No solution found!";
         }
 
-        public override string Solve_1() => $"Solution to {ClassPrefix} {CalculateIndex()}, part 1";
+        public override string Solve_2()
+        {
+            var data = _input;
+            data.Sort();
 
-        public override string Solve_2() => $"Solution to {ClassPrefix} {CalculateIndex()}, part 2";
+            int upper, lower;
+
+            for (int i = 0; i < data.Count - 1; i++)
+            {
+                lower = i + 1;
+                upper = data.Count - 1;
+                while (lower < upper)
+                {
+                    if (data.ElementAt(i) + data.ElementAt(lower) + data.ElementAt(upper) == target)
+                        return (data.ElementAt(i) * data.ElementAt(lower) * data.ElementAt(upper)).ToString();
+                    if (data.ElementAt(i) + data.ElementAt(lower) + data.ElementAt(upper) < target) lower++; else upper--;
+                }
+            }
+            return "No solution found!";
+        }
     }
 }
---
+++ AdventOfCode/Day_02.cs
@@ -0,0 +1,62 @@
+﻿using AoCHelper;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public class Day_02 : BaseDay
+    {
+        private readonly string[] _input;
+        private const int target = 2020;
+
+        public Day_02()
+        {
+            _input = File.ReadAllLines(InputFilePath);
+        }
+        public override string Solve_1()
+        {
+            uint nValid = 0;
+            string[] currentLine;
+            char[] separators = new char[] { ' ', '.', '-', ':' };
+            foreach (var line in _input)
+            {
+                currentLine = line.Split(separators, System.StringSplitOptions.RemoveEmptyEntries);
+                if (isValidPasswordOld(currentLine[3], int.Parse(currentLine[0]), int.Parse(currentLine[1]), currentLine[2])) nValid++;
+            }
+            return nValid.ToString();
+        }
+
+        private bool isValidPasswordOld(string password, int minLength, int maxLength, string character)
+        {
+            var n = 0;
+            foreach (var letter in password)
+            {
+                if (letter == character[0]) n++;
+                if (n > maxLength) return false;
+            }
+            if (n < minLength) return false;
+            return true;
+        }
+
+        public override string Solve_2()
+        {
+            uint nValid = 0;
+            string[] currentLine;
+            char[] separators = new char[] { ' ', '.', '-', ':' };
+            foreach (var line in _input)
+            {
+                currentLine = line.Split(separators, System.StringSplitOptions.RemoveEmptyEntries);
+                if (isValidPasswordNew(currentLine[3], int.Parse(currentLine[0]), int.Parse(currentLine[1]), currentLine[2])) nValid++;
+            }
+            return nValid.ToString();
+        }
+
+        private bool isValidPasswordNew(string password, int firstPos, int secondPos, string character)
+        {
+            if ((password[firstPos - 1] == character[0] && password[secondPos - 1] != character[0]) || (password[firstPos - 1] != character[0] && password[secondPos - 1] == character[0]))
+                return true;
+            return false;
+        }
+    }
+}
