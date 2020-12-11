a3cc6442c954526a39a5aa05dab8a37db7bc9a81
Fabian Emmel <emmelnem@gmail.com>
2020-12-11 16:17:07 +0100

    Add Day 10, 11

---
+++ AdventOfCode/Day_10.cs
@@ -0,0 +1,63 @@
+﻿using AoCHelper;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public class Day_10 : BaseDay
+    {
+        private readonly List<int> _input;
+        private List<int> _output;
+        private const int target = 2020;
+
+        public Day_10()
+        {
+            if (!File.Exists(InputFilePath))
+            {
+                throw new SolvingException($"Path {InputFilePath} not found for {GetType().Name}");
+            }                   
+            _input = File.ReadAllLines(InputFilePath).Select(int.Parse).ToList();
+        }
+        public override string Solve_1()
+        {
+            _input.Sort();
+            _output = new();
+            for(int i = 1; i < _input.Count; i++)
+            {
+                _output.Add(_input[i] - _input[i - 1]);
+            }
+            return ((_output.Where(x => x == 1).Count() + 1) * (_output.Where(x => x == 3).Count() + 1)).ToString();            
+        }
+
+        public override string Solve_2()
+        {
+            var indexIs3 = _output.Select((v, i) => new { v, i }).Where(x => x.v == 3).Select(x => x.i).ToList();
+            var diff3 = new List<int>();
+
+            for (int i = 1; i < indexIs3.Count; i++)
+            {
+                diff3.Add(indexIs3[i] - indexIs3[i - 1]);
+            }
+            diff3 = diff3.Where(x => x > 1).ToList();
+            diff3 = diff3.Select(x => x - 1).ToList();
+            var result = 2L;
+            foreach(var value in diff3)
+            {
+                switch(value)
+                {
+                    case 2:
+                        result *= 2;
+                        break;
+                    case 3:
+                        result *= 4;
+                        break;
+                    case 4:
+                        result *= 7;
+                        break;
+                }
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ AdventOfCode/Day_11.cs
@@ -0,0 +1,155 @@
+﻿using AoCHelper;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public class Day_11 : BaseDay
+    {
+        private readonly string[] _input;
+        private char[,] currentGeneration;
+        private int[,] neighbours;
+        bool changed;
+
+        public Day_11()
+        {
+            if (!File.Exists(InputFilePath))
+            {
+                throw new SolvingException($"Path {InputFilePath} not found for {GetType().Name}");
+            }
+            _input = File.ReadAllLines(InputFilePath);
+            currentGeneration = new char[_input.Length, _input[0].Length];
+            neighbours = new int[_input.Length, _input[0].Length];
+        }
+
+        private void parseInput()
+        {
+            for (int i = 0; i < currentGeneration.GetLength(0); i++)
+            {
+                for (int j = 0; j < currentGeneration.GetLength(1); j++)
+                {
+                    currentGeneration[i, j] = _input.ElementAt(i).ElementAt(j);
+                }
+            }
+        }
+
+        public override string Solve_1()
+        {
+            parseInput();
+            changed = true;
+            while (changed)
+            {                
+                Evolve(0, 4, false);
+            }
+            return CountOccupiedSeats().ToString();
+        }
+
+        private void CountAllNeighbours(bool method)
+        {
+            for (int i = 0; i < currentGeneration.GetLength(0); i++)
+            {           
+                for (int j = 0; j < currentGeneration.GetLength(1); j++)
+                {
+                    neighbours[i, j] = method? CountVisibleNeighbours(i, j) : CountDirectNeighbours(i, j);
+                }
+            }
+        }
+
+        private void Evolve(int minNeighbours, int maxNeighbours, bool method)
+        {
+            CountAllNeighbours(method);
+
+            changed = false;
+
+            for (int i = 0; i < currentGeneration.GetLength(0); i++)
+            {
+                for (int j = 0; j < currentGeneration.GetLength(1); j++)
+                {
+                    if (currentGeneration[i, j] == 'L' && neighbours[i, j] == minNeighbours)
+                    {
+                        currentGeneration[i, j] = '#';
+                        changed = true;
+                    }
+                    else if (currentGeneration[i, j] == '#' && neighbours[i, j] >= maxNeighbours)
+                    {
+                        currentGeneration[i, j] = 'L';
+                        changed = true;
+                    }
+                }
+            }
+        }
+
+        private int CountOccupiedSeats()
+        {
+            var seats = from char seat in currentGeneration where seat == '#' select seat;
+            return seats.Count();
+
+        }
+
+        private int CountDirectNeighbours(int i, int j)
+        {
+            if (currentGeneration[i, j] == '.') return 0;
+            var sum = 0;
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    if (i + x < 0 || i + x >= currentGeneration.GetLength(0) || j + y < 0 || j + y >= currentGeneration.GetLength(1) || (x == 0 && y == 0)) ;
+                    else
+                    {
+                        if (currentGeneration[i + x, j + y] == '#') sum++;
+                    }
+                }
+            }
+            return sum;
+        }
+
+        private int CountVisibleNeighbours(int i, int j)
+        {
+            if (currentGeneration[i, j] == '.') return 0;
+
+            var sum = 0;
+
+            sum += CheckDirection(i, j, -1, 0);
+            sum += CheckDirection(i, j, 1, 0);
+            sum += CheckDirection(i, j, 0, -1);
+            sum += CheckDirection(i, j, 0, 1);
+            sum += CheckDirection(i, j, 1, 1);
+            sum += CheckDirection(i, j, 1, -1);
+            sum += CheckDirection(i, j, -1, 1);
+            sum += CheckDirection(i, j, -1, -1);
+
+            return sum;
+        }
+
+        private int CheckDirection(int x, int y, int dx, int dy)
+        {
+            var mul = 1;
+            while(true)
+            {
+                if (x + mul * dx < 0 || x + mul * dx >= currentGeneration.GetLength(0) || y + mul * dy < 0 || y + mul * dy >= currentGeneration.GetLength(1)) return 0;
+                else
+                {
+                    switch(currentGeneration[x + mul * dx, y + mul * dy])
+                    {
+                        case 'L': return 0;
+                        case '#': return 1;
+                    }
+                }
+                mul++;
+            }
+        }
+
+        public override string Solve_2()
+        {
+            parseInput();
+
+            changed = true;
+            while (changed)
+            {
+                Evolve(0, 5, true);
+            }
+            return CountOccupiedSeats().ToString();
+        }
+    }
+}
